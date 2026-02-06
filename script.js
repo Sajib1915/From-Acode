b2e15d5714e3dc2951a3a83930e323a3b06a64cc
Sajib1915 <mohammadsajibulislam2024@gmail.com>
2026-02-06 16:09:17 +0600

    update script and add image

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const number = [1,2,3,4,5,6,7,8]
 console.log(number.length)
+
+var manName = ['Karim','Rapiq','Jabbar','Shamim','Ramim']
+//আউটপুট দেখতে
+console.log(manName)
+//অ্যারে থেকে নির্দিষ্ট আইটেম এক্সেস করা 
+console.log(manName[0])
+console.log(manName[1])
+//কয়টা আইটেম আছে জানতে/এটাকে ডট নোটেশন ও বলে-.length
+console.log(manName.length)
+
+
+
+
